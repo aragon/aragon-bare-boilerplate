3bb10b27fe34932c15d1e5be25976ce43c0e5e54
Gabriel <gabrielpk.18@gmail.com>
2019-12-15 05:24:10 -0300

    Use rpc network and  move to truffle 5

--- truffle.js
+++ truffle.js
@@ -1,3 +1,1 @@
-// Node.js 11 fix (https://github.com/aragon/aragon-cli/issues/731)
-module.exports = require('@aragon/os/truffle-config')
-module.exports.solc.optimizer.enabled = false
+module.exports = require('@aragon/truffle-config-v5')
